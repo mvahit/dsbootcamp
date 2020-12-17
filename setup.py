0628ca0f2d4686260fcf5d7a289853fc9600d5ba
mvahit <m.vahitkeskin@gmail.com>
2020-12-17 20:04:26 +0100

    setup file'den once son cıkıs

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(name="dsbootcamp",
+                 version="0.0.1",
+                 license="MIT",
+                 author="Vahit Keskin",
+                 author_mail="m.vahitkeskin",
+                 description="Data Science Tools Package",
+                 url="https://github.com/mvahit/dsbootcamp",
+                 package=setuptools.find_packages(),
+                 keywords=["data science", "machine learning", "bootcamp"],
+                 python_requires=">= 3.6"
+                 )
